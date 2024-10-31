27b240b3bd24bac695f00ac9210260c969a6c34b
Abdala <clahimoha1000@gmail.com>
2024-10-31 23:21:01 +0300

    Created isolation of files.

---
+++ code/stage/data-stores/mysql/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.5"
+    }
+  }
+  required_version = ">= 1.7"
+
+  # backend "s3" {
+  #   # The bucket we are going to store our state
+  #   bucket = "terraform-state-bucket-saldf234"
+  #   key = "stage/data-stores/mysql/terraform.tfstate"
+  #   region = "us-east-1"
+
+  #   # The dynamoDB used to lock the state
+  #   dynamodb_table = "terraform-locks"
+  #   encrypt = true
+  # }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "db_instance" {
+  identifier_prefix = "terraform-db-instance"
+  engine = "mysql"
+  allocated_storage = 10
+  instance_class = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name = "terraform_database"
+
+  username = var.db_username
+  password = var.db_password
+}
